675629c97b902bdb7fc9732f3d3239afbc3d7ca9
andrewsouzandress20-lab <andrewsouzandress20@gmail.com>
2026-02-04 12:35:05 +0000

    chore: remover arquivos .js antigos para rodar só ts-node puro

--- test_telegram_send.js
+++
@@ -1,6 +0,0 @@
-import 'dotenv/config';
-import { sendTelegramSignal } from './services/telegramService.js';
-(async () => {
-    await sendTelegramSignal('US30', 'COMPRA', 'FORTE', 10);
-    console.log('Teste de envio de mensagem para o Telegram finalizado.');
-})();
--- utils/fetchMarketOpenTimes.js
+++
@@ -1,5 +0,0 @@
-// Utilitário para buscar horários de abertura do arquivo local
-export async function fetchMarketOpenTimes() {
-    const res = await fetch('/market_open_times.json');
-    return await res.json();
-}
