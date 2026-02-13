d79722eb571bf22717e47e3889cefdc4589d9718
andrewsouzandress20-lab <andrewsouzandress20@gmail.com>
2026-02-13 20:11:59 +0000

    Load mobile.css for mobile layout fixes

--- index.tsx
+++ index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.js';
 import ErrorBoundary from './components/ErrorBoundary.js';
+import './mobile.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
